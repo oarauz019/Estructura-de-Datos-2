94b4eff23b08bdd9753954d0310ac3818a1c363c
NRF95 <ramirezfuentes.nahum@gmail.com>
2025-06-05 16:33:19 -0600

    update

---
+++ src/main/java/com/mycompany/proyectofinal/MenuLogica.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectofinal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nahum
+ */
+public class MenuLogica {
+/**
+ * Esta clase permite gestionar la logica del menu y su navegacion para poder utilzar las opciones solicitadas
+ * para que el usuario pueda selecionar las opciones apropiadas para moverse por el sistema y ser atendido
+ * @author nahum
+ */
+    public void menu() {
+        Scanner scanner = new Scanner(System.in);
+        int opciones;
+        System.out.println("¡Bienvenido al Hopital Metropolitano!");
+        do {
+            System.out.println("\nMenú Principal:");
+            System.out.println("1. Gestionar Llegada de Pacientes");
+            System.out.println("2. Ayuda");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opciones = scanner.nextInt();
+            switch (opciones) {
+                case 1:
+                    System.out.println("Opcion: Llegada de pacientes.");
+                    break;
+                case 2:
+                    System.out.println("Opcion: Ayuda.");
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa. ¡Gracias por su visita!");
+                    break;
+                default:
+                    System.out.println("Opcion invalida.");
+            }
+        } while (opciones != 3);
+
+        scanner.close();
+    }
+}
+
+
+
+    
