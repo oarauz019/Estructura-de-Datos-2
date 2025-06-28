bcb40e66dfc7cbb77d58aed63b930f9e60bc3bd1
NRF95 <ramirezfuentes.nahum@gmail.com>
2025-06-28 14:02:09 -0600

    Creacion de clases paciente, cola y nodo. Adicional agregar los metodos en la clase de Gestion pacientes (mostrar pacientes / asignacion de pacientes).

--- src/main/java/com/mycompany/proyectofinal/GestionPacientes.java
+++ src/main/java/com/mycompany/proyectofinal/GestionPacientes.java
@@ -5,6 +5,7 @@
 package com.mycompany.proyectofinal;
 
 import java.util.Scanner;
+import java.util.Date;
 
 /**
  * 
@@ -12,10 +13,10 @@ import java.util.Scanner;
  */
 public class GestionPacientes {
     private MenuLogica MenuPrincipal = new MenuLogica();
-    private Cola c_Regular = new Cola();
-    private int conta_Regular = 1;
-    private Cola c_Prefe = new Cola();
-    private int conta_Prefe = 1;
+    private Cola c_Regular = new Cola(); // Cola pacientes regulares
+    private int conta_Regular = 1; // Contador de pacientes regulares
+    private Cola c_Prefe = new Cola(); // Cola pacientes preferenciles
+    private int conta_Prefe = 1; // Contador pacientes preferenciales
  
 /**
  * Esta clase permite la llegada de pacientes al centro de salud
@@ -76,18 +77,57 @@ public class GestionPacientes {
                     break;
             }
 
-        } while (opcion != 6);
-
-        scanner.close();
-    
+        } while (opcion != 6);  
     }
-     private void mostrarFichasPendientes() {
-        System.out.println("Fichas sin atender en la categoria preferencial: ---");
-        c_Prefe.imprimirCola();
+/**
+     * En este metodo se le va a solicar al usuario los datos del paciente y para de esta manera poder delegar una ficha según la categoria del mismo
+     * Finalmente el sistema segun lo indicado se va a colocar al paciente en la cola correspondiente.
+     * @Autor: Nahum Ramirez
+*/
+    private void escogerFichaPaciente(Scanner scanner) {
+        System.out.println("Escoge la categoria apropiada de ficha: ");
+        System.out.println("1. Paciente Preferencial");
+        System.out.println("2. Paciente Regular");
+        System.out.print("Categoria: ");
 
+        int categoria = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Ingrese número de cédula: ");
+        String cedula = scanner.nextLine();
+        scanner.nextLine();
+        
+        System.out.print("Ingrese nombre completo del paciente: ");
+        String nombre = scanner.nextLine();
+
+        Date fecha = new Date();
+        String ficha;
+
+        if (categoria == 1){
+            ficha = "P" + conta_Prefe++;
+            Paciente paciente = new Paciente(fecha, nombre, categoria, ficha);
+            c_Prefe.encolar(paciente);
+        } else if (categoria == 2) {
+            ficha = "R" + conta_Regular++;
+            Paciente paciente = new Paciente(fecha, nombre, categoria, ficha);
+            c_Regular.encolar(paciente);
+        } else {
+            System.out.println("Categoria de ficha no válida.");
+            return;
+        }
+
+        System.out.println("Su número de ficha es la: " + ficha);
+    }
+/**
+     * Metodo que nos permite mostrar la ficha de los pacientes
+     * Nos muestra las fichas sin atender
+     * @Autor: Nahum Ramirez
+*/    
+     private void mostrarFichasPendientes() {
         System.out.println("Fichas sin atender en la categoria regular: ");
         c_Regular.imprimirCola();
+        
+        System.out.println("Fichas sin atender en la categoria preferencial: ");
+        c_Prefe.imprimirCola();
     }   
-    
-    
 }
